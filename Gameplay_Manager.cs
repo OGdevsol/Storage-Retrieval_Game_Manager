b8746ae9b52ec948b09d3a08356be9f2dad8617f
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2021-12-01 12:36:27 +0500

    Add files via upload

---
+++ Gameplay_Manager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameplay_Manager : MonoBehaviour
+{
+    public GameObject[] LevelM1;
+    public GameObject[] LevelM2;
+    public GameObject[] character;
+    private int currentLevel;
+    private int pressCount;
+    
+    
+    void Start()
+    {
+        pressCount = 0;
+        ActivateModeAndLevel();
+        activateSelectedCharacter();
+        InitiationDebug();
+        
+    }
+
+    private void Update()
+    {
+        pressFunctionality();
+        levelFunctionality();
+    }
+
+    public void activateSelectedCharacter()
+    {
+        character[PlayerPrefs.GetInt("selectedcharacter")].SetActive(true);
+        //Will activate the selected character with the help of a character[] array where characters are stored.
+        
+    }
+    public void ActivateModeAndLevel()
+    {
+        if (PlayerPrefs.GetString("Mode")=="Mode1")
+        {
+            LevelM1[PlayerPrefs.GetInt("SelectedLevelM1")].SetActive(true);
+            //PlayerPrefs.GetInt("M1PressLimit");
+            currentLevel = PlayerPrefs.GetInt("SelectedLevelM1");
+            UI_Manager.instance.M1pressLimitAssign(PlayerPrefs.GetInt("M1PressLimit"));
+
+        }
+        else if (PlayerPrefs.GetString("Mode")=="Mode2")
+        {
+            LevelM2[PlayerPrefs.GetInt("SelectedLevelM2")].SetActive(true);
+            currentLevel = PlayerPrefs.GetInt("SelectedLevelM2");
+            UI_Manager.instance.M2pressLimitAssign(PlayerPrefs.GetInt("M2PressLimit"));
+        }
+        //This function will simply check for the saved "Mode". Whether the player clicked on "Mode1" or "Mode2". 
+        // The level[] will then be activated using the playerprefs that saved the level data, which level was clicked 
+        // and the clicked level will be activated. "currentLevel" is set to be same as the value of selected level, it can
+        // be later used for other purposes such as level lock and unlock system, level objectives assignment etc.
+    }
+
+    public void InitiationDebug()
+    {
+        Debug.Log(PlayerPrefs.GetString("SelectedCharacter"));
+        Debug.Log("Index for selected character is "+ PlayerPrefs.GetInt("selectedcharacter"));
+        Debug.Log("Selected Mode is  "+ PlayerPrefs.GetString("Mode"));
+        Debug.Log("Selected Level Index== "+ currentLevel);
+        Debug.Log("Press Count is " + pressCount);
+    }
+
+    public void pressFunctionality()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Pressed");
+            pressCount++;
+            Debug.Log(pressCount);
+            if (PlayerPrefs.GetString("Mode")=="Mode1")
+            {
+                Debug.Log(PlayerPrefs.GetInt("M1PressLimit"));
+            }
+            else if(PlayerPrefs.GetString("Mode")=="Mode2")
+            {
+                Debug.Log(PlayerPrefs.GetInt("M2PressLimit"));
+            }
+            //This is a basic level functionality. Press xyz key to increment the presscount, which will be used to check if
+            //it has reached the press count limit.
+        }
+    }
+
+    public void levelFunctionality()
+    {
+        if (PlayerPrefs.GetString("Mode")=="Mode1")
+        {
+            if (pressCount==PlayerPrefs.GetInt("M1PressLimit"))
+            {
+                Debug.Log("M1 Level Complete");
+            }
+        }
+        else if (PlayerPrefs.GetString("Mode")=="Mode2")
+        {
+            if (pressCount==PlayerPrefs.GetInt("M2PressLimit"))
+            {
+                Debug.Log("M2 Level Complete");
+            }
+        }
+        //Level Complete functionality. Level is completed based on user input, each spacebar press increments the presscount,
+        //when presscount reaches presscount limit for that level, level is completed.
+        
+    }
+
+    public void lvLCompletePanelNextBTN()
+    {
+        if (PlayerPrefs.GetString("Mode")=="Mode1")
+        {
+            Debug.Log(" Click Functional For Mode 1");
+            //PlayerPrefs.SetInt("SelectedLevelM1",PlayerPrefs.GetInt("SelectedLevelM1"+1));
+            LevelM1[PlayerPrefs.GetInt("SelectedLevelM1")].SetActive(false);
+            PlayerPrefs.SetInt("SelectedLevelM1",PlayerPrefs.GetInt("SelectedLevelM1")+1);
+            SceneManager.LoadScene("Gameplay");
+            //PlayerPrefs.GetInt("M1PressLimit");
+            LevelM1[PlayerPrefs.GetInt("SelectedLevelM1")+1].SetActive(true);
+            PlayerPrefs.SetInt("M1PressLimit",PlayerPrefs.GetInt("M1PressLimit")+1);
+            //UI_Manager.instance.M1pressLimitAssign(PlayerPrefs.GetInt("M1PressLimit"));
+        }
+        else if (PlayerPrefs.GetString("Mode")=="Mode2")
+        {
+            Debug.Log(" Click Functional For Mode 2");
+            //PlayerPrefs.SetInt("SelectedLevelM1",PlayerPrefs.GetInt("SelectedLevelM1"+1));
+            LevelM2[PlayerPrefs.GetInt("SelectedLevelM2")].SetActive(false);
+            PlayerPrefs.SetInt("SelectedLevelM2",PlayerPrefs.GetInt("SelectedLevelM2")+1);
+            SceneManager.LoadScene("Gameplay");
+            //PlayerPrefs.GetInt("M1PressLimit");
+            LevelM1[PlayerPrefs.GetInt("SelectedLevelM2")+1].SetActive(true);
+            PlayerPrefs.SetInt("M2PressLimit",PlayerPrefs.GetInt("M2PressLimit")+1);
+            //UI_Manager.instance.M1pressLimitAssign(PlayerPrefs.GetInt("M1PressLimit"));
+        }
+    }
+
+    public void HomeButtonClick()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+   
+    
+    
+}
---
+++ UI_Manager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public static UI_Manager instance;
+    public GameObject mainMenu;
+    public GameObject p_modeSelection;
+    public GameObject pm1_LevelSelection;
+    public GameObject pm2_LevelSelection;
+    public GameObject p_CharacterSelection;
+    public GameObject p_m1Lvls;
+    public GameObject p_m2Lvls;
+    public Button[] Mode1LvLs;
+    public Button[] Mode2Lvls;
+
+    private void Awake()
+    {
+        if (instance==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void CharacterSelection()
+    {
+        p_CharacterSelection.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void selectedCharacterRed()
+    {
+        PlayerPrefs.SetString("SelectedCharacter", "Red"); 
+        PlayerPrefs.SetInt("selectedcharacter",0); //Saving player data with playerprefs which is later retrieved in  
+        Debug.Log("Pressed Red");           //Gameplay_Manager to get the selected character which is red with index 0.
+        p_modeSelection.SetActive(true);
+        p_CharacterSelection.SetActive(false);
+        
+        
+       
+    }
+    public void selectedCharacterGreen()
+    {
+        PlayerPrefs.SetString("SelectedCharacter", "Green");
+        PlayerPrefs.SetInt("selectedcharacter",1);  //Saving player data with playerprefs which is later retrieved in
+        Debug.Log("Pressed Green");          //Gameplay_Manager to get the selected character which is green with index 1.
+        p_modeSelection.SetActive(true);
+        p_CharacterSelection.SetActive(false);
+    }
+    public void selectedCharacterBlue()
+    {
+        PlayerPrefs.SetString("SelectedCharacter", "Blue");
+        PlayerPrefs.SetInt("selectedcharacter",2); //Saving player data with playerprefs which is later retrieved in
+        Debug.Log("Pressed Blue");          //Gameplay_Manager to get the selected character which is blue with index 2.
+        p_modeSelection.SetActive(true);
+        p_CharacterSelection.SetActive(false);
+    }
+
+    public void mode1Selection()
+    {
+        PlayerPrefs.SetString("Mode","Mode1");   // Saving Mode which is Mode1 if clicked on this button, it is later retrieved to activate mode 1 levels.
+        p_m1Lvls.SetActive(true);
+    }
+    
+    public void mode2Selection()
+    {
+        PlayerPrefs.SetString("Mode", "Mode2"); // Saving Mode which is Mode2 if clicked on this button, it is later retrieved to activate mode 2 levels.
+        p_m2Lvls.SetActive(true);
+    }
+
+    public void m1LevelSelection(int a)
+    {
+        PlayerPrefs.GetString("Mode");
+        PlayerPrefs.SetInt("SelectedLevelM1",a);  //Stored "Mode" playerprefs is retrieved, level index data is saved using playerprefs
+        SceneManager.LoadScene("Gameplay"); //A parameter is passed to this function, when level buttons are linked with it, they will require an int index. 
+    }
+    
+    public void m2LevelSelection(int b)
+    {
+        PlayerPrefs.GetString("Mode");
+        PlayerPrefs.SetInt("SelectedLevelM2",b); //Stored "Mode" playerprefs is retrieved, level index data is saved using playerprefs
+        SceneManager.LoadScene("Gameplay"); //A parameter is passed to this function, when level buttons are linked with it, they will require an int index. 
+    }
+
+    public void characterSelectionBack()
+    {
+        p_CharacterSelection.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void modeSelectionBack()
+    {
+        p_modeSelection.SetActive(false);
+        p_CharacterSelection.SetActive(true);                    // Activating and deactivating panels based on back button click.
+    }                                                             // Activating and deactivating panels based on back button click.
+
+    public void M1levelSelectionBack()
+    {
+        p_m1Lvls.SetActive(false);
+        p_modeSelection.SetActive(true);
+    }
+    public void M2levelSelectionBack()
+    {
+        p_m2Lvls.SetActive(false);
+        p_modeSelection.SetActive(true);
+    }
+    
+    public void M1pressLimitAssign(int M1pressLimit)
+    {
+        PlayerPrefs.GetString("Mode");
+        PlayerPrefs.SetInt("M1PressLimit",M1pressLimit);
+        //A presscount limit for each level is defined for mode 1 using an int parameter. The data is then saved using playerprefs.
+        // press limit count is same as defined by the user in the inspector using the parameter/button link.
+        //It is retrieved in gameplay manager to implement level/gameplay functionality.
+    }
+
+    public void M2pressLimitAssign(int M2pressLimit)
+    {
+        PlayerPrefs.GetString("Mode");
+        PlayerPrefs.SetInt("M2PressLimit",M2pressLimit);
+        //A presscount limit for each level is defined for mode 2 using an int parameter. The data is then saved using playerprefs.
+        // press limit count is same as defined by the user in the inspector using the parameter/button link.
+        //It is retrieved in gameplay manager to implement level/gameplay functionality.
+    }
+    
+    
+}
