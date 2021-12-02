6d39cde74f3964c1aa402b1cc99074235564076b
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2021-12-02 14:14:39 +0500

    Add files via upload

---
+++ coinMove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinMove : MonoBehaviour
+{
+    public float rotationVal=50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,rotationVal*Time.deltaTime,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            Destroy(gameObject);
+            Gameplay_Manager.instance.increment();
+        }
+    }
+}
